e0d72cc1be4fead732f3d61aad4c4a797c60191b
Iain Dooley <iain@workingsoftware.com.au>
2011-09-22 09:00:58 +1000

    Created initial repo

---
+++ args.class.php
@@ -0,0 +1,70 @@
+<?php
+    class Args
+    {
+        const argv = 'argv';
+        
+        public static function arrayFilter()
+        {
+            return function($arg)
+            {
+                if(is_array($arg) && !count($arg))
+                    $arg = FALSE;
+
+                return $arg;
+            };
+        }
+
+        public static function get($name)
+        {
+            $ret = NULL;
+            //get dynamica arguments
+            $args = func_get_args();
+            //shift off the $name argument
+            array_shift($args);
+            //throw an exception if they didn't pass anything
+            //except the name
+            if(!count($args))
+                throw new InvalidArgumentsRequestException('You tried to get '.$name.' from Args::get but you didn\'t pass in any argument arrays (eg. $_POST, $_GET, or Args::argv');
+            $filter = self::arrayFilter();
+            //loop through the args
+            foreach($args as $arg)
+            {
+                //if they passed in a filter
+                if($arg instanceof Closure)
+                    $filter = $arg;
+
+                else
+                {
+                    if($arg == self::argv)
+                        $arg = self::buildArgv();
+                        
+                    if($ret === NULL)
+                        if(isset($arg[$name]))
+                            $ret = $arg[$name];
+                }
+            }
+
+            return $filter($ret);
+        }
+
+        private static function buildArgv()
+        {
+            global $argv;
+            $ret = array();
+
+            foreach($argv as $arg)
+            {
+                if(strpos($arg,'=') !== FALSE)
+                {
+                    $split = explode('=',$arg);
+                    $name = $split[0];
+                    $value = $split[1];
+                    $ret[$name] = $value;
+                }
+            }
+            
+            return $ret;
+        }
+    }
+    
+    class InvalidArgumentsRequestException extends Exception{}
---
+++ test_args.class.php
@@ -0,0 +1,49 @@
+<?php
+    class TestArgs implements rocketsled\Runnable
+    {
+        public function run()
+        {
+            $mysql_filter = function($arg)
+            {
+                $ret = NULL;
+                $chain = Args::arrayFilter();
+                
+                if($ret = $chain($arg))
+                    //NOTE: you would actually use mysql_real_escape_string()
+                    //here, but we don't want to require a db for this test
+                    $ret = addslashes($arg);
+
+                return $ret;
+            };
+            
+            $_POST = array('name' => 'some value',
+                           'empty_array' => array(),
+                           'an_array' => array('ohai' => 'guy'),
+                           );
+            $_GET = array('another_name' => 'some value',
+                          'escaped' => 'something\'s rotten');
+
+            echo 'name: '.Args::get('name',$_POST,$_GET,Args::argv).PHP_EOL;
+            echo 'name from argv: '.Args::get('name',Args::argv).PHP_EOL;
+            echo 'another_name: '.Args::get('another_name',$_POST,$_GET).PHP_EOL;
+            echo 'escaped: '.Args::get('escaped',$_POST,$_GET,Args::argv,$mysql_filter).PHP_EOL;
+            echo 'empty array default filter: '.Args::get('empty_array',$_POST).PHP_EOL;
+            echo 'empty array custom filter: '.PHP_EOL;
+            print_r(Args::get('empty_array',$_POST,function($arg){return $arg;})).PHP_EOL;
+            echo 'full array default filter: '.PHP_EOL;
+            print_r(Args::get('an_array',$_POST)).PHP_EOL;
+            echo 'full array custom filter: '.PHP_EOL;
+            print_r(Args::get('an_array',$_POST,function($arg){return $arg;})).PHP_EOL;
+            
+            try
+            {
+                Args::get('ohai');
+                echo '****FAILED**** why didn\'t fetching with only a name throw InvalidArgumentsRequestException?'.PHP_EOL;
+            }
+            
+            catch(InvalidArgumentsRequestException $exc)
+            {
+                echo '****PASSED**** calling Args::get with only a name threw InvalidArgumentsRequestException'.PHP_EOL;
+            }
+        }
+    }
